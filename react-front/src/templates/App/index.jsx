cf993b2fd7f27680f9237f836f6b2c7f6faf1ca3
Matheus <94717377+Mati-Pereira@users.noreply.github.com>
2022-07-08 14:34:17 -0300

    update front end app
    updating the react part

---
+++ react-front/src/templates/App/index.jsx
@@ -0,0 +1,13 @@
+import { Div } from './styles'
+
+function Home() {
+  return (
+    <div className="container">
+      <Div>
+        <h1>Olá</h1>
+      </Div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ react-front/src/templates/App/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Div = styled.div`
+    background-color: azure;
+    padding: 2rem;
+    border: 1px solid #000;
+    background-color: red;
+`
